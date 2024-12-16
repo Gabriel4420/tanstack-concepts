9642330dea54b2202fdebf40d93bf460d9141943
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-12-16 13:38:53 -0300

    ultimas atualizações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
-'use client'
+"use client";
 
-import { usePosts } from '@/utils/queries';
-import { Post } from '../../types/post';
+import { InvalidatePosts, usePosts } from "@/utils/queries";
+import { Post } from "../../types/post";
+import { useUsersPrefetch } from "@/utils/queries";
 
 export default function HomePage() {
+  
 
-  const { data, isLoading } = usePosts()
+  const handleINvalidate = () => {
+     InvalidatePosts()
+  };
 
-  if (isLoading) return <div>Carregando...</div>
+  useUsersPrefetch();
+
+  const { data, isLoading } = usePosts();
+
+  if (isLoading) return <div>Carregando...</div>;
 
   return (
-    <div className="flex items-center justify-center bg-gray-100">
-      <div className="flex flex-col gap-4">
+    <div className="flex flex-col items-center justify-center bg-gray-400">
+      <h1 className="text-3xl font-bold">Posts</h1>
+      <div className="flex flex-col gap-2 my-3 p-2 border border-black">
+        <p className="block">Area de inserção de novos posts</p>
+        <button className="bg-slate-800 rounded-md px-2 text-white" onClick={handleINvalidate}>Invalidar</button>
+      </div>
+      <div className="flex flex-col gap-2">
         {data?.map((post: Post) => (
-          <div key={post.id}>{post.title}</div>
+          <p className="text-base" key={post.id}>{post.title}</p>
         ))}
       </div>
     </div>
--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { api } from "./api"
+
 
 export const usePosts = () => {
   return useQuery({
@@ -13,4 +14,24 @@ export const usePostById = (id: number) => {
     queryKey: ['posts', id],
     queryFn: () => api.posts.getById(id)
   })
+}
+
+export const useUsers = () => {
+  return useQuery({
+    queryKey: ['users'],
+    queryFn: api.users.getAll
+  })
+}
+
+export const useUsersPrefetch = () => {
+  const queryClient = useQueryClient()
+  return queryClient.prefetchQuery({
+    queryKey: ['users'],
+    queryFn: api.users.getAll
+  })
+}
+
+export const InvalidatePosts = () => {
+  const queryClient = useQueryClient()
+  return queryClient.invalidateQueries({ queryKey: ['posts'], exact: true })
 }
